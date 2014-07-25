d71620f1e9b9155c57ece17eb51863cc2637decc
Kenan Yildirim <kenan@kenany.me>
2014-07-25 18:54:15 -0400

    Remove extraneous `console.log`
    Closes #6

--- bin/cmd.js
+++ bin/cmd.js
@@ -52,6 +52,4 @@ function next() {
 
   var guess = jotto.bestGuess(words, possibleWords);
   console.log(printf('%22s : %6s', chalk.cyan('Best guess'), chalk.bold.green(guess)));
-
-  console.log(possibleWords);
 }
